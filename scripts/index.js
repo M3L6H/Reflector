117cf613e0897fb88c81d711c1f24143a5047948
M3L6H <sisoac@hotmail.com>
2020-07-13 15:35:44 -0700

    Added void

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import Tile from "./tiles/reflector.js";
-import Placeable from './tiles/obstacle.js';
+import Placeable from './tiles/void.js';
 
 const createErrorMsg = (body) => {
   const msg = document.createElement("p");
---
+++ scripts/tiles/void.js
@@ -0,0 +1,12 @@
+import Tile from "./tile.js";
+
+class Void extends Tile {
+  constructor(x, y, size) {
+    super(x, y, size);
+    this.colorLight = "#000000";
+    this.color = "#000000";
+    this.colorDark = "#000000";
+  }
+}
+
+export default Void;
