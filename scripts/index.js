4f239b2a973d6fa6b67dd6940b2382b479f12686
M3L6H <sisoac@hotmail.com>
2020-07-13 15:27:56 -0700

    Added end tiles

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import Tile from "./tiles/reflector.js";
-import Placeable from './tiles/spawner.js';
+import Placeable from './tiles/end.js';
 
 const createErrorMsg = (body) => {
   const msg = document.createElement("p");
---
+++ scripts/tiles/end.js
@@ -0,0 +1,62 @@
+import Tile from "./tile.js";
+
+class Reflector extends Tile {
+  constructor(x, y, size) {
+    super(x, y, size);
+    this.colorLight = "#8D0147";
+    this.color = "#6A0136";
+    this.colorDark = "#650133";
+    this.colorX = "#C20C2D";
+    this.xWidth = this.size / 8;
+    this.elapsed = 0;
+    this.speed = 0.25;
+  }
+
+  update({ ctx, delta }) {
+    Tile.prototype.update.apply(this, arguments);
+    this.elapsed = (this.elapsed + delta * this.speed) % 360;
+
+    ctx.save();
+    ctx.translate(this.x + this.size / 2, this.y + this.size / 2);
+    ctx.rotate(Math.PI / 180 * (this.elapsed));
+
+    ctx.strokeStyle = this.colorX;
+    ctx.lineWidth = this.xWidth;
+
+    // Draw X
+    ctx.beginPath();
+    ctx.moveTo(-this.xWidth, -this.xWidth);
+    ctx.lineTo(this.xWidth, this.xWidth);
+    ctx.moveTo(this.xWidth, -this.xWidth);
+    ctx.lineTo(-this.xWidth, this.xWidth);
+    ctx.stroke();
+
+    ctx.restore();
+
+    ctx.save();
+    ctx.translate(this.x + this.size / 2, this.y + this.size / 2);
+    ctx.fillStyle = this.colorX;
+
+    if (this.elapsed > 270) {
+      ctx.beginPath();
+      ctx.arc(-2 * this.xWidth, 2 * this.xWidth, this.xWidth, 0, 2 * Math.PI);
+      ctx.fill();
+    } else if (this.elapsed > 180) {
+      ctx.beginPath();
+      ctx.arc(2 * this.xWidth, 2 * this.xWidth, this.xWidth, 0, 2 * Math.PI);
+      ctx.fill();
+    } else if (this.elapsed > 90) {
+      ctx.beginPath();
+      ctx.arc(2 * this.xWidth, -2 * this.xWidth, this.xWidth, 0, 2 * Math.PI);
+      ctx.fill();
+    } else {
+      ctx.beginPath();
+      ctx.arc(-2 * this.xWidth, -2 * this.xWidth, this.xWidth, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+      
+    ctx.restore();
+  }
+}
+
+export default Reflector;
