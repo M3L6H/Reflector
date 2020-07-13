5edae8921afc126211aa59a3562a30835e75e965
M3L6H <sisoac@hotmail.com>
2020-07-13 09:59:32 -0700

    Render loop

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,8 @@ const createErrorMsg = (body) => {
   return msg;
 }
 
+let canvas, ctx, width, height, unit;
+
 // Initialization
 const initialize = () => {
   const root = document.getElementById("root");
@@ -15,7 +17,7 @@ const initialize = () => {
     root.appendChild(createErrorMsg("your screen is not wide enough to play this game!"));
     return;
   } else {
-    const unit = Math.floor(winWidth / 20);
+    unit = Math.floor(winWidth / 20);
     const usableWidth = unit * 20;
     const targetHeight = unit * 12;
   
@@ -24,17 +26,36 @@ const initialize = () => {
       return;
     }
 
-    const canvas = document.createElement("canvas");
+    canvas = document.createElement("canvas");
     canvas.width = usableWidth;
     canvas.height = targetHeight;
     canvas.classList.add("canvas");
+
+    ctx = canvas.getContext("2d");
+    width = canvas.width;
+    height = canvas.height;
     
     root.appendChild(canvas);
   }
   
   // Let all the elements of the game know we have finished initialization
-  const init = new Event("Init", { unit, width: usableWidth, height: targetHeight });
+  const init = new Event("Init", { unit, width, height });
   document.dispatchEvent(init);
+  window.requestAnimationFrame(render);
+};
+
+let start = 0;
+
+// Render loop
+const render = (time) => {
+  const delta = (!start && 0) + (start && (time - start));
+  start = time;
+
+  ctx.clearRect(0, 0, width, height);
+  
+  const update = new Event("Update", { delta, ctx, width, height });
+  document.dispatchEvent(update);
+  window.requestAnimationFrame(render);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
