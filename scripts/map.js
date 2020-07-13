883a04192165d9189dfc29ad34cfd2911aacf091
M3L6H <sisoac@hotmail.com>
2020-07-13 16:58:39 -0700

    Got path animating beautifully

--- scripts/map.js
+++ scripts/map.js
@@ -31,13 +31,62 @@ class Map {
       });
     });
 
+    this.elapsed = 0;
+    this.speed = 0.2;
+    this.colorPath = "rgba(0, 255, 217, 0.1)";
+    this.pathWidth = 4;
+    this.pathLength = 8;
+    
     document.addEventListener("Update", ({ detail }) => this.update(detail));
   }
 
-  update({ delta }) {
+  calculateDir(x, y) {
+    if (x === 0) {
+      return [-1, y];
+    } else if (y === 0) {
+      return [x, -1];
+    } else if (x === 19) {
+      return [20, y]
+    } else {
+      return [x, 12];
+    }
+  }
+
+  drawPath(ctx, unit, offset=0) {
+    const elapsed = (this.elapsed + offset) % 1000;
+
+    for (let start in this.paths) {
+      const path = this.paths[start];
+      const tile = path[Math.floor(elapsed / 1000 * path.length)];
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.strokeStyle = this.colorPath;
+      ctx.lineWidth = this.pathWidth;
+      ctx.moveTo(path[0][0] * unit + unit / 2, path[0][1] * unit + unit / 2);
+      for (let i = 0; i < path.length; ++i) {
+        const [x, y] = path[i];
+        if (tile[0] === x && tile[1] === y) {
+          ctx.lineTo(x * unit + unit / 2, y * unit + unit / 2);
+        } else {
+          ctx.moveTo(x * unit + unit / 2, y * unit + unit / 2);
+        }
+      }
+      ctx.stroke();
+      ctx.restore();
+    }
+  }
+
+  update({ delta, unit, ctx }) {
+    this.elapsed = (this.elapsed + delta * this.speed) % 1000;
+
     this.map.forEach(row => {
       row.forEach(tile => tile.update(...arguments))
     });
+
+    for (let i = 0; i < 10; ++i) {
+      this.drawPath(ctx, unit, 8 * i);
+    }
   }
 }
 
