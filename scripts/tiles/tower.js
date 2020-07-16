6c0d99b2fc683f18173a052c8d73489ff1e73328
M3L6H <sisoac@hotmail.com>
2020-07-16 11:19:51 -0700

    Got laser bolts working

--- scripts/tiles/tower.js
+++ scripts/tiles/tower.js
@@ -1,6 +1,7 @@
 import Tile from './tile.js';
 import Vector from '../physics/vector.js';
 import Ray from '../physics/ray.js';
+import normalize from "../util/normalize.js";
 
 import debouncer from '../util/debouncer.js';
 
@@ -15,6 +16,7 @@ class Tower extends Tile {
     this.colorLight = "#A2B3B9";
     this.color = "#97ABB1";
     this.colorDark = "#8BA1A7";
+    this.laserBolts = [];
 
     this.baseColor = "#2B2D42";
     this.baseDark = "#020202";
@@ -24,6 +26,7 @@ class Tower extends Tile {
         this.speed = 0.8;
         this.laserLength = unit;
         this.laserWidth = unit / 6;
+        this.laserColor = "rgba(8, 164, 189, 0.8)";
         this.orbColor = "#08A4BD";
         this.orbBorder = "#446DF6";
         break;
@@ -31,18 +34,21 @@ class Tower extends Tile {
         this.speed = 0.4;
         this.laserLength = unit * 3 / 4;
         this.laserWidth = unit / 8;
+        this.laserColor = "rgba(255, 63, 120, 0.8)";
         this.orbColor = "#FF3F00";
         this.orbBorder = "#FF7F11";
         break;
       case "green":
         this.speed = 1.4;
         this.laserWidth = unit / 8;
+        this.laserColor = "rgba(134, 205, 130, 0.8)";
         this.orbColor = "#86CD82";
         this.orbBorder = "#57A773";
         setInterval(() => this.emitting = !this.emitting, this.speed * 1000);
         break;
       case "yellow":
         this.laserWidth = unit / 5;
+        this.laserColor = "rgba(242, 220, 93, 0.8)";
         this.orbColor = "#F2DC5D";
         this.orbBorder = "#ED9B40";
         break;
@@ -98,10 +104,12 @@ class Tower extends Tile {
 
     // If we have a length, we are a pulse, otherwise we are a beam
     if (this.laserLength) {
-      
+      this.emitting = false;
+      setTimeout(() => this.emitting = true, this.speed * 1000);
+      this.laserBolts.push({ pos: new Vector(this.x + unit / 2, this.y + unit / 2), currNode: 0 });
     } else {
       // If we have a speed, we aren't constantly firing
-      ctx.strokeStyle = this.orbColor;
+      ctx.strokeStyle = this.laserColor;
       ctx.lineWidth = this.laserWidth;
       ctx.beginPath();
       ctx.moveTo(this.x + unit / 2, this.y + unit / 2);
@@ -110,6 +118,58 @@ class Tower extends Tile {
         ctx.lineTo(rayHit.hitPoint.x, rayHit.hitPoint.y);
       }
       ctx.stroke();
+
+      ctx.lineWidth = this.laserWidth / 2;
+      ctx.beginPath();
+      ctx.moveTo(this.x + unit / 2, this.y + unit / 2);
+      for (let i = 0; i < this.ray.numCollisions; ++i) {
+        const rayHit = this.ray.collisions[i];
+        ctx.lineTo(rayHit.hitPoint.x, rayHit.hitPoint.y);
+      }
+      ctx.stroke();
+    }
+  }
+
+  drawLaserBolts({ ctx, delta, unit }) {
+    for (let i = 0; i < this.laserBolts.length; ++i) {
+      const that = this.laserBolts[i];
+
+      const target = this.ray.collisions[that.currNode].hitPoint;
+  
+      const [dirX, dirY] = normalize([target.x - that.pos.x, target.y - that.pos.y]);
+      
+      that.pos.x += dirX * delta;
+      that.pos.y += dirY * delta;
+  
+      // Correct overshooting
+      if (Math.sign(target.x - that.pos.x) !== Math.sign(dirX)) {
+        that.pos.x = target.x;
+      }
+  
+      if (Math.sign(target.y - that.pos.y) !== Math.sign(dirY)) {
+        that.pos.y = target.y;
+      }
+  
+      if (that.pos.x === target.x && that.pos.y === target.y) {
+        that.currNode += 1;
+        
+        if (that.currNode >= this.ray.collisions.length) {
+          this.laserBolts.splice(i, 1);
+          --i;
+        }
+      }
+
+      const endPoint = (new Vector(dirX, dirY)).inv().scale(this.laserLength);
+
+      ctx.save();
+      ctx.translate(that.pos.x, that.pos.y);
+      ctx.lineWidth = this.laserWidth;
+      ctx.strokeStyle = this.laserColor;
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(endPoint.x, endPoint.y);
+      ctx.stroke();
+      ctx.restore();
     }
   }
 
@@ -125,6 +185,7 @@ class Tower extends Tile {
     // Draw laser
     if (this.aimed) {
       this.emit(ctx, unit);
+      this.drawLaserBolts(...arguments);
     } else {
       ctx.strokeStyle = "#FF0000";
       ctx.setLineDash([4, 15]);
