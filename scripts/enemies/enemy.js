0dd69ef6cf3c69a16b867c5015ccf1452393b50f
M3L6H <sisoac@hotmail.com>
2020-07-16 12:08:37 -0700

    Gave enemies a better graphic and a collider

--- scripts/enemies/enemy.js
+++ scripts/enemies/enemy.js
@@ -1,4 +1,7 @@
+import Collider from '../physics/collider.js';
+import Vector from '../physics/vector.js';
 import normalize from '../util/normalize.js';
+import clamp from '../util/clamp.js';
 
 class Enemy {
   constructor(path, unit) {
@@ -8,13 +11,20 @@ class Enemy {
     this.y = this.path[this.currNode][1] * unit;
     this.speed = 20;
 
+    this.collider = new Collider(new Vector(this.x + unit / 2, this.y + unit / 2), 0, [new Vector(-unit * 0.4, -unit * 0.4), new Vector(unit * 0.4, -unit * 0.4), new Vector(unit * 0.4, unit * 0.4), new Vector(-unit * 0.4, unit * 0.4)], "enemies");
+
+    this.maxHealth = 100;
     this.health = 100;
+    this.armor = 0;
     this.flying = false;
     this.shielded = false;
     this.reflectorUp = false;
     this.reflectorRight = false;
     this.reflectorDown = false;
     this.reflectorLeft = false;
+
+    this.colorBase = "#0C090D";
+    this.color = "#E01A4F";
   }
 
   update({ ctx, delta, unit }) {
@@ -43,11 +53,28 @@ class Enemy {
     if (Math.sign(targetY - this.y) !== Math.sign(dirY)) {
       this.y = targetY;
     }
+
+    this.collider.updatePos(new Vector(this.x + unit / 2, this.y + unit / 2));
     
     ctx.save();
     ctx.translate(this.x + unit / 2, this.y + unit / 2);
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(-5, -2, 10, 4);
+    ctx.fillStyle = this.colorBase;
+
+    ctx.beginPath();
+    ctx.moveTo(-unit * 0.4, -unit * 0.4);
+    ctx.lineTo(unit * 0.4, -unit * 0.4);
+    ctx.lineTo(unit * 0.4, unit * 0.4);
+    ctx.lineTo(-unit * 0.4, unit * 0.4);
+    ctx.fill();
+
+    // Draw rings
+    const numRings = 3;
+    for (let i = 0; i < Math.ceil(this.health / this.maxHealth) * numRings; ++i) {
+      ctx.strokeStyle = this.color;
+      ctx.beginPath();
+      ctx.arc(0, 0, unit * (0.3 - 0.1 * i), 0, clamp((this.health - i * this.maxHealth / numRings) / (this.maxHealth / numRings)) * 2 * Math.PI);
+      ctx.stroke();
+    }
     ctx.restore();
   }
 }
