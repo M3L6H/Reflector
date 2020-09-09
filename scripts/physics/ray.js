6c2e35d29ea5c1d5c5a684c9a49be4687fa3e29d
M3L6H <sisoac@hotmail.com>
2020-09-09 12:24:01 -0700

    Update raycast with comments

--- scripts/physics/ray.js
+++ scripts/physics/ray.js
@@ -110,49 +110,69 @@ class Ray extends Collider {
 
   // Modified ray marching algorithm that bounces around
   bounceCast(bounceLayer) {
-    this.collisions = [];
+    this.collisions = []; // Track all the collisions we encounter
     this.steps = [];
     this.numCollisions = 0;
+
+    // We use the first vector in our model as the direction
     let dir = this.model[0].unit();
     let dist = 0;
     let point = this.pos;
+
+    // Apply the first step of the ray marching algorithm
+    // Calculate the minimum distance to the closest collider
     let min = this.minDistToLine(point);
 
-    let steps = 1000;
+    let steps = 1000; // We don't want the algorithm to continue forever
 
+    // We cap the ray both on distance and number of steps
     while (dist < 10000 && steps > 0) {
       steps--;
       this.steps.push([point, min]);
 
+      // min represents the distance to our closest collider
+      // If it is less than some threshold, we consider ourselves to be colliding
+      // Using a threshold is important to prevent accidentally going through a
+      // collider instead of bouncing off it
       if (min <= precision) {
-        let exit = true;
+        let exit = true; // Assume we are done tracing
 
+        // Iterate through our most recent collisions
         this.lastHits.forEach(lastCollision => {
           const lastHit = lastCollision.colliderHit;
+          // We only collide with colliders in the specified bounce layer
           if (lastHit.layer === bounceLayer) {
-            exit = false;
-            const [a, b] = lastCollision.edgeHit;
-            const edge = a.sub(b);
-            let norm = edge.normal();
+            exit = false; // We collided, so we must continue calculating
 
+            // Get the endpoints of the edge we collided with
+            const [a, b] = lastCollision.edgeHit;
+            
+            // Calculate the vector representing the edge
+            const edge = a.sub(b);
+            let norm = edge.normal(); // Get the normal of said edge
+
+            // To avoid issues with flipped normals
             if (norm.dot(dir) > 0) {
               norm = norm.inv();
             }
 
+            // Calculate the bounce direction
             let newDir = dir.vectorProj(edge).sub(dir.vectorProj(norm));
   
-            dir = newDir.unit();
+            dir = newDir.unit(); // Set the bounce direction
 
+            // Increase the current min enough to "escape" the current edge
             if (min <= precision) min = 5 * precision;
           }
         });
 
-        if (exit) break;
+        if (exit) break; // Stop calculating if we did not collide
       }
 
+      // Step in the current direction by the current minimum
       point = point.add(dir.scale(min));
-      dist += min;
-      min = this.minDistToLine(point);
+      dist += min; // Add to our distance
+      min = this.minDistToLine(point); // Recalculate the minimum distance
     }
   }
 }
