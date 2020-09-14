6aecbdb1029ef27a7a185ad6657a483fa0750fa9
M3L6H <sisoac@hotmail.com>
2020-09-13 22:18:01 -0700

    Add default values

--- scripts/util/storage.js
+++ scripts/util/storage.js
@@ -1,14 +1,14 @@
-import { APP_NAME } from './constants';
+import { APP_NAME } from './constants.js';
 
 const getKey = key => `${ APP_NAME }/${ key }`;
 
 const getAppKeys = () => {
-  const appDict = JSON.parse(localStorage.getItem(getKey("__appkeys__")));
+  const appDict = JSON.parse(localStorage.getItem(getKey("__appkeys__"))) || {};
   return Object.keys(appDict);
 };
 
 const addToAppKeys = key => {
-  const appDict = JSON.parse(localStorage.getItem(getKey("__appkeys__")));
+  const appDict = JSON.parse(localStorage.getItem(getKey("__appkeys__"))) || {};
   appDict[key] = true;
   localStorage.setItem(getKey("__appkeys__"), JSON.stringify(appDict));
 };
