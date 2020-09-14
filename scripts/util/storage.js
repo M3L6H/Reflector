5dfee47d0bbb25748d2600a17db786cfe87a97f1
M3L6H <sisoac@hotmail.com>
2020-09-13 22:10:00 -0700

    Add ability to clear namespaced local storage

--- scripts/util/storage.js
+++ scripts/util/storage.js
@@ -2,5 +2,30 @@ import { APP_NAME } from './constants';
 
 const getKey = key => `${ APP_NAME }/${ key }`;
 
+const getAppKeys = () => {
+  const appKeys = localStorage.getItem(getKey(";"));
+  return appKeys.split(";");
+};
+
+const addToAppKeys = key => {
+  if (key.includes(";")) {
+    throw "Key names cannot include a semi-colon!";
+  }
+
+  const appDict = {};
+  getAppKeys().forEach(key => appDict[key] = true);
+  appDict[getKey(key)] = true;
+
+  localStorage.setItem(getKey(";"), Object.keys(appDict).join(";"));
+};
+
 export const getItem = key => localStorage.getItem(getKey(key));
-export const setItem = (key, value) => localStorage.setItem(getKey(key), value);
+
+export const setItem = (key, value) => {
+  addToAppKeys(key);
+  return localStorage.setItem(getKey(key), value);
+};
+
+export const clear = () => {
+  getAppKeys().forEach(key => localStorage.removeItem(key));
+};
